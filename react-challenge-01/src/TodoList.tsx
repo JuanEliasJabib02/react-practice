acdc4501da581382433fe8ea4abd65e907c3ffa2
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-10-21 00:54:31 -0500

    tailwind installed

---
+++ react-challenge-01/src/TodoList.tsx
@@ -0,0 +1,10 @@
+export default function TodoList() {
+	return (
+		<div className='bg-red-500 h-screen'>
+			<div>
+				<div>TODO SCREEN</div>
+				<div>TODO ITEM</div>
+			</div>
+		</div>
+	);
+}
